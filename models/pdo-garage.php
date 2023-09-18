b7828d34862ba18070248ef03a3959d83ac2c4d1
Plastaaa <80416635+Plastaaa@users.noreply.github.com>
2023-09-18 22:01:33 +0200

    ADMIN VOITURE V1

--- models/pdo-garage.php
+++ models/pdo-garage.php
@@ -111,7 +111,155 @@ class pdoGarage {
     $result = $prepa->fetchAll(\PDO::FETCH_ASSOC);
     return $result;
   }
+
+  public static function getLastVoiture(){
+    $request = "SELECT ID FROM VOITURES ORDER BY ID DESC LIMIT 1;";
+    $prepa = self::$monPdo->prepare($request);
+    $prepa->execute();
+    $result = $prepa->fetchColumn();
+    return $result;
+  }
+
+
+  public static function addVoiture($marque, $modele, $annee, $energie, $trans, $prix, $motor, $km, $porte, $place, $couleur, $cvfisc, $cvdin, $equip1, $equip2, $equip3, $equip4, $equip5, $equip6, $equip7, $equip8, $equip9, $equip10){
+    $connexionGarage = pdoGarage::getPdoGarage();
+    $request = "INSERT INTO `VOITURES` (`ID`, `MARQUE`, `MODELE`, `ANNEE`, `ENERGIE`, `TRANSMISSION`, `MOTORISATION`, `PRIX`, `KILOMETRAGE`, `PORTES`, `PLACES`, `COULEUR`, `CVFISC`, `CVR`) 
+      VALUES (:id, :marque, :modele, :annee, :energie, :trans, :motor, :prix, :km, :porte, :place, :couleur, :cvfisc, :cvdin);";
+
+    $prepa = self::$monPdo->prepare($request);
+
+    $prepa->bindValue(':id', null);
+    $prepa->bindValue(':marque', $marque);
+    $prepa->bindValue(':modele', $modele);
+    $prepa->bindValue(':annee', $annee);
+    $prepa->bindValue(':energie', $energie);
+    $prepa->bindValue(':trans', $trans);
+    $prepa->bindValue(':motor', $motor);
+    $prepa->bindValue(':prix', $prix);
+    $prepa->bindValue(':km', $km);
+    $prepa->bindValue(':porte', $porte);
+    $prepa->bindValue(':place', $place);
+    $prepa->bindValue(':couleur', $couleur);
+    $prepa->bindValue(':cvfisc', $cvfisc);
+    $prepa->bindValue(':cvdin', $cvdin);
+
+    $prepa->execute();
+
+    $result = $prepa-> rowCount();
+
+    $resReqGet = $connexionGarage->getLastVoiture();
+
+    $request = "INSERT INTO `EQUIPEMENTS` (`ID`, `LIBELLE`, `VOITURE_ID`) VALUES (NULL, :equip1, :id),(NULL, :equip2, :id),(NULL, :equip3, :id),(NULL, :equip4, :id),(NULL, :equip5, :id),(NULL, :equip6, :id),(NULL, :equip7, :id),(NULL, :equip8, :id),(NULL, :equip9, :id),(NULL, :equip10, :id);";
+
+    $prepa = self::$monPdo->prepare($request);
+
+    $prepa->bindValue(':id', $resReqGet);
+    $prepa->bindValue(':equip1', $equip1);
+    $prepa->bindValue(':equip2', $equip2);
+    $prepa->bindValue(':equip3', $equip3);
+    $prepa->bindValue(':equip4', $equip4);
+    $prepa->bindValue(':equip5', $equip5);
+    $prepa->bindValue(':equip6', $equip6);
+    $prepa->bindValue(':equip7', $equip7);
+    $prepa->bindValue(':equip8', $equip8);
+    $prepa->bindValue(':equip9', $equip9);
+    $prepa->bindValue(':equip10', $equip10);
+
+    $prepa->execute();
+
+    $result = $prepa-> rowCount();
+
+    return $result;
+  }
+
+  public static function updateVoiture($id, $marque, $modele, $annee, $energie, $trans, $prix, $motor, $km, $porte, $place, $couleur, $cvfisc, $cvdin, $equip1, $equip2, $equip3, $equip4, $equip5, $equip6, $equip7, $equip8, $equip9, $equip10){
+    $connexionGarage = pdoGarage::getPdoGarage();
+
+    $request = "UPDATE `VOITURES` SET `MARQUE` = :marque, `MODELE` = :modele, `ANNEE` = :annee, `ENERGIE` = :energie, `TRANSMISSION` = :trans, `MOTORISATION` = :motor, `PRIX` = :prix, `KILOMETRAGE` = :km, `PORTES` = :porte, `PLACES` = :place, `COULEUR` = :couleur, `CVFISC` = :cvfisc, `CVR` = :cvdin WHERE `VOITURES`.`ID` = :id;";
+    $prepa = self::$monPdo->prepare($request);
+
+    $prepa->bindValue(':marque', $marque);
+    $prepa->bindValue(':modele', $modele);
+    $prepa->bindValue(':annee', $annee);
+    $prepa->bindValue(':energie', $energie);
+    $prepa->bindValue(':trans', $trans);
+    $prepa->bindValue(':motor', $motor);
+    $prepa->bindValue(':prix', $prix);
+    $prepa->bindValue(':km', $km);
+    $prepa->bindValue(':porte', $porte);
+    $prepa->bindValue(':place', $place);
+    $prepa->bindValue(':couleur', $couleur);
+    $prepa->bindValue(':cvfisc', $cvfisc);
+    $prepa->bindValue(':cvdin', $cvdin);
+    $prepa->bindValue(':id', $id);
+
+    $prepa->execute();
+
+    $result = $prepa-> rowCount();
+
+    $equipement = $connexionGarage->getEquipement($id);
+
+    print_r($equipement);
+
+    $request = "UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip10 WHERE `EQUIPEMENTS`.`ID` = $equipement[0]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip1 WHERE `EQUIPEMENTS`.`ID` = $equipement[1]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip2 WHERE `EQUIPEMENTS`.`ID` = $equipement[2]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip3 WHERE `EQUIPEMENTS`.`ID` = $equipement[3]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip4 WHERE `EQUIPEMENTS`.`ID` = $equipement[4]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip5 WHERE `EQUIPEMENTS`.`ID` = $equipement[5]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip6 WHERE `EQUIPEMENTS`.`ID` = $equipement[6]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip7 WHERE `EQUIPEMENTS`.`ID` = $equipement[7]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip8 WHERE `EQUIPEMENTS`.`ID` = $equipement[8]['ID']; 
+    UPDATE `EQUIPEMENTS` SET `LIBELLE` = :equip9 WHERE `EQUIPEMENTS`.`ID` = $equipement[9]['ID'];";
+
+    $prepa = self::$monPdo->prepare($request);
+
+    $prepa->bindValue(':equip1', $equip1);
+    $prepa->bindValue(':equip2', $equip2);
+    $prepa->bindValue(':equip3', $equip3);
+    $prepa->bindValue(':equip4', $equip4);
+    $prepa->bindValue(':equip5', $equip5);
+    $prepa->bindValue(':equip6', $equip6);
+    $prepa->bindValue(':equip7', $equip7);
+    $prepa->bindValue(':equip8', $equip8);
+    $prepa->bindValue(':equip9', $equip9);
+    $prepa->bindValue(':equip10', $equip10);
+
+    $prepa->execute();
+
+    $result = $prepa-> rowCount();
+
+    return $result;
+  }
+
+  public static function deleteVoiture($id){
+    $request = "DELETE FROM VOITURES WHERE ID='".$id."'";
+    $prepa = self::$monPdo->prepare($request);
+    $prepa->execute();
+
+    $request = "DELETE FROM EQUIPEMENTS WHERE VOITURE_ID='".$id."'";
+    $prepa = self::$monPdo->prepare($request);
+    $prepa->execute();
+
+    $result = $prepa-> rowCount();
+    return $result;
+  }
+
+  public static function getVoiture($id){
+    $request = "SELECT * FROM VOITURES WHERE ID = $id";
+    $prepa = self::$monPdo->prepare($request);
+    $prepa->execute();
+    $result = $prepa->fetchAll(\PDO::FETCH_ASSOC);
+    return $result;
+  }
+
+  public static function getEquipement($id){
+    $request = "SELECT * FROM EQUIPEMENTS WHERE VOITURE_ID = $id";
+    $prepa = self::$monPdo->prepare($request);
+    $prepa->execute();
+    $result = $prepa->fetchAll(\PDO::FETCH_ASSOC);
+    return $result;
+  }
+
 }
-
-
 ?>
--- vues/dashboard/liste-voitures.php
+++ vues/dashboard/liste-voitures.php
@@ -5,11 +5,20 @@
     <!-- Start block -->
       <section class="p-3 sm:p-5 antialiased">
         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
-          <div class="text-center w-32 mb-4">
-            <div class="bg-clifford text-white p-2 rounded-lg shadow-md hover:bg-red-700">
-              <a href="index.php?controleur=gestionadmin&action=formaddvoiture">
-                Ajouter
-              </a>
+          <div class="inline-flex">
+            <div class="text-center w-32 shadow-xl">
+              <div class="bg-clifford text-white p-2 rounded-tl-lg hover:bg-red-700">
+                <a href="index.php?controleur=gestionadmin&action=formaddvoiture">
+                  Ajouter
+                </a>
+              </div>
+            </div>
+            <div class="text-center w-32 shadow-xl">
+              <div class="bg-blue-500 text-white p-2 rounded-tr-lg hover:bg-blue-700">
+                <a href="index.php?controleur=gestionadmin&action=formaddvoiture">
+                  Horaires
+                </a>
+              </div>
             </div>
           </div>
           <div class="bg-white relative shadow-md sm:rounded-lg overflow-hidden">                        
@@ -22,15 +31,12 @@
                     <th scope="col" class="p-4">Modèle</th>
                     <th scope="col" class="p-4">Année</th>
                     <th scope="col" class="p-4">Energie</th>
-                    <th scope="col" class="p-4">Transmission</th>
-                    <th scope="col" class="p-4">Motorisation</th>
+                    
                     <th scope="col" class="p-4">Prix</th>
                     <th scope="col" class="p-4">Kilométrage</th>
-                    <th scope="col" class="p-4">Portes</th>
-                    <th scope="col" class="p-4">Places</th>
                     <th scope="col" class="p-4">Couleur</th>
                     <th scope="col" class="p-4">Chevaux fiscaux</th>
-                    <th scope="col" class="p-4">Chevaux réels</th>
+                    <th scope="col" class="p-4">Actions</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -43,25 +49,25 @@
                       <th scope="col" class="p-4"><?php echo $voiture['MODELE'] ?></th>
                       <th scope="col" class="p-4"><?php echo $voiture['ANNEE'] ?></th>
                       <th scope="col" class="p-4"><?php echo $voiture['ENERGIE'] ?></th>
-                      <th scope="col" class="p-4"><?php echo $voiture['TRANSMISSION'] ?></th>
-                      <th scope="col" class="p-4"><?php echo $voiture['MOTORISATION'] ?></th>
                       <th scope="col" class="p-4"><?php echo $voiture['PRIX'] ?></th>
                       <th scope="col" class="p-4"><?php echo $voiture['KILOMETRAGE'] ?></th>
-                      <th scope="col" class="p-4"><?php echo $voiture['PORTES'] ?></th>
-                      <th scope="col" class="p-4"><?php echo $voiture['PLACES'] ?></th>
                       <th scope="col" class="p-4"><?php echo $voiture['COULEUR'] ?></th>
                       <th scope="col" class="p-4"><?php echo $voiture['CVFISC'] ?></th>
-                      <th scope="col" class="p-4"><?php echo $voiture['CVR'] ?></th>
-                      <th scope="col" class="p-4">
-                        <form method="post" action="#">
-                          <input type="hidden" value=<?php echo "\"".$voiture['ID']."\"" ?> name="id"/>
-                            <button type="submit">
-                              Supprimer
-                            </button>
-                        </form>
-                        <button>
-                          Modifier
-                        </button>
+                      <th scope="col" class="">
+                        <div class="inline-flex">
+                          <form method="post" action="index.php?controleur=gestionadmin&action=supprimervoiture">
+                            <input type="hidden" value=<?php echo "\"".$voiture['ID']."\"" ?> name="id"/>
+                              <button type="submit" class="p-2 bg-clifford text-white rounded-l-lg">
+                                Supprimer
+                              </button>
+                          </form>
+                          <form method="post" action="index.php?controleur=gestionadmin&action=formmodifiervoiture">
+                            <input type="hidden" value=<?php echo "\"".$voiture['ID']."\"" ?> name="id"/>
+                              <button type="submit" class="p-2 bg-blue-500 text-white rounded-r-lg">
+                                Modifier
+                              </button>
+                          </form>
+                        </div>
                       </th>
                     </tr>
                   <?php
