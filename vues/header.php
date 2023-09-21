5f06af3891af19425de9867f6c1d4ec5fca756c7
Chloe-Patte <patte.chloe.cp@gmail.com>
2023-09-21 22:50:06 +0200

    v.0.9.5

--- vues/header.php
+++ vues/header.php
@@ -3,7 +3,7 @@
   
     <div class="w-16 h-16">
       <a href="index.php"> <img src="http://nunesaccount.alwaysdata.net/test/garagevparrot.png" alt="Logo du site">
-</a>
+      </a>
     </div>
     <div class="flex md:order-2">
       <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
